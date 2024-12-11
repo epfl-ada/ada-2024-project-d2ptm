e3371ee0f2ed44cd59301ace67aa2e0760cd4f8c
Tim <t.titarenko@simcord.com>
2024-12-11 10:19:37 +0100

    Add q3

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ import shutil
 from pathlib import Path
 
 import pandas as pd
-import wget
+# import wget
 
 URL = {
     "dataset": "http://www.cs.cmu.edu/~ark/personas/data/MovieSummaries.tar.gz",
@@ -12,18 +12,46 @@ URL = {
 
 ROOT_PATH = Path(__file__).absolute().resolve().parent.parent
 DATA_PATH = ROOT_PATH / "data" / "cmu"
+AWARD_PATH = ROOT_PATH / "data" / "awards"
 
 
-def download_data(force_download=False):
-    if not DATA_PATH.exists() or force_download:
-        DATA_PATH.mkdir(exist_ok=True, parents=True)
+# def download_data(force_download=False):
+#     if not DATA_PATH.exists() or force_download:
+#         DATA_PATH.mkdir(exist_ok=True, parents=True)
+#
+#         wget.download(URL["readme"], str(DATA_PATH / "README.txt"))
+#         wget.download(URL["dataset"], str(DATA_PATH / "MovieSummaries.tar.gz"))
+#
+#         shutil.unpack_archive(str(DATA_PATH / "MovieSummaries.tar.gz"), str(DATA_PATH))
+#         os.remove(str(DATA_PATH / "MovieSummaries.tar.gz"))
 
-        wget.download(URL["readme"], str(DATA_PATH / "README.txt"))
-        wget.download(URL["dataset"], str(DATA_PATH / "MovieSummaries.tar.gz"))
 
-        shutil.unpack_archive(str(DATA_PATH / "MovieSummaries.tar.gz"), str(DATA_PATH))
-        os.remove(str(DATA_PATH / "MovieSummaries.tar.gz"))
+def load_awards():
+    """
+    Load the movie awards dataset.
+    Assumes the awards data contains 'FreebaseMovieId', 'Awards', and 'Nominations' columns.
 
+    Args:
+    - file_path (str): Path to the awards dataset.
+
+    Returns:
+    - DataFrame: Pandas DataFrame containing the awards data.
+    """
+    awards = pd.read_csv(AWARD_PATH / "awards_actors.tsv",  sep="\t")
+    return awards
+
+def load_nominations():
+    """
+    Load the movie nominations dataset, assuming it has columns: 'freebase_ids' and 'nominations' (comma-separated nominations).
+
+    Args:
+    - file_path (str): Path to the nominations dataset.
+
+    Returns:S
+    - DataFrame: Pandas DataFrame containing the nominations data.
+    """
+    nominations = pd.read_csv(AWARD_PATH / "nominations_actors.tsv",  sep="\t")
+    return nominations
 
 def load_plots():
     """Returns a pandas DataFrame containing plot summaries."""
